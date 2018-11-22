04964af6ebc04ad2932cf723eebc8a5a0b461d61
monangas <jhon.morenog@misena.edu.co>
2018-11-22 08:54:21 -0500

    Creación de la clase Conexion y agregación de la librería de MySQL

---
+++ src/java/com/tenniskiss/conexion/Conexion.java
@@ -0,0 +1,74 @@
+package com.tenniskiss.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexion {
+
+    private Connection conexion;
+    private Statement statement;
+
+    public Conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            this.conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis", "root", "");
+            this.statement = this.conexion.createStatement();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+public void cerrarConexion(){
+        try {
+            this.getStatement().close();
+            this.getConexion().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+    public boolean CUD(String sql){
+        boolean b=false;
+        try {
+          b=this.getStatement().execute(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return b;
+    }
+    public ResultSet consultar(String sql) {
+        ResultSet r = null;
+        try {
+            r = this.getStatement().executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return r;
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    public Statement getStatement() {
+        return statement;
+    }
+
+    public void setStatement(Statement statement) {
+        this.statement = statement;
+    }
+
+}
